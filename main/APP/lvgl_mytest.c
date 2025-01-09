2d291fe9ee565695aa09038cd68f13e116ad6343
Administrator <2350808537@qq.com>
2025-01-09 14:56:00 +0800

    <feat>:LVGL之滚动条的使用

--- main/APP/lvgl_mytest.c
+++ main/APP/lvgl_mytest.c
@@ -5,28 +5,26 @@
 #include "lvgl_mytest.h"
 #include "XL9555.h"
 
-const lv_point_t line_points[] = {{10,20},{70,50}, {120,10}, {140,60},{180,10}};    /*坐标点集合*/
-#define LINE_POINTS_NUM     (sizeof(line_points)/sizeof(line_points[0]))   /*坐标点的个数*/
-lv_obj_t *line1;
+#define MAX_VALUE 100
+#define MIN_VALUE 0
+
+static void slider_event_cb(lv_event_t * e);
 
 //例程入口函数
 void lv_line_test_start()
 {
-    lv_obj_t *scr = lv_scr_act();
+    /*Create a slider in the center of the display*/
+    lv_obj_t * slider;
+    slider = lv_slider_create( lv_scr_act() );
+    lv_obj_center(slider);
 
-    //创建自定义样式
-    static lv_style_t line_style;
-    lv_style_init(&line_style);
-    lv_style_set_line_color(&line_style, lv_color_hex(0xff0000));
-    lv_style_set_line_width(&line_style,4);
-    lv_style_set_line_rounded(&line_style,1);
+    lv_slider_set_mode(slider, LV_SLIDER_MODE_RANGE);
+    lv_slider_set_range(slider, MIN_VALUE, MAX_VALUE);
+    lv_slider_set_value(slider, 70, LV_ANIM_OFF);
+    lv_slider_set_left_value(slider, 20, LV_ANIM_OFF);
 
-
-    //创建线条对象
-    line1 = lv_line_create(scr);
-    lv_obj_set_pos(line1,20,20);
-    lv_line_set_points(line1,line_points,LINE_POINTS_NUM);
-    lv_obj_add_style(line1,&line_style,0);
+    lv_obj_add_event_cb(slider, slider_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_refresh_ext_draw_size(slider);
 }
 
 //案件处理
@@ -34,10 +32,7 @@ void key_handler(void)
 {
     uint8_t key = xl9555_key_scan(0);
 
-    if ( key == KEY0_PRES )
-    {
-        lv_line_set_y_invert(line1, !lv_line_get_y_invert(line1));
-    }
+
 }
 
 
